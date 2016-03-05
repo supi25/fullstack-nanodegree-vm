f37c4eff042da8c970a2fa97e78cfa39415e0043
unknown <dont@emailme.ever>
2016-03-05 15:49:39 -0500

    Added templates and empty forms to the project.

--- vagrant/working/restaurants/finalProject.py
+++ vagrant/working/restaurants/finalProject.py
@@ -1,41 +1,51 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+#Fake Restaurants
+restaurant = {'name': 'Sandras Tea Parlor', 'id': '1'}
+
+restaurants = [{'name': 'Sandras Tea Parlor', 'id': '1'}, {'name': 'Ryans specialty pesto', 'id': '2'}, {'name': 'The waffle cart', 'id': '3'}]
+#restaurants = []
+
+items = [{'name':'Earl grey', 'description':'not grey, but good', 'price':'$2.99', 'course':'Side', 'id':'1'}, {'name':'Pepermint', 'description':'Ryans favorite', 'price':'$2.50', 'course':'Side', 'id':'2'}, {'name':'Basil Pesto', 'description':'standard pesto', 'price':'$4.99', 'course':'Side', 'id':'3'}, {'name':'Cashew Pesto', 'description':'nutty', 'price':'$5.99', 'course':'Side', 'id':'4'}, {'name':'Belgian Waffle', 'description':'With real maple syrup', 'price':'$3.50', 'course':'Entree', 'id':'5'}, {'name':'Chocolate Waffle', 'description':'Covered in delicious chocolate', 'price':'$4.50', 'course':'Entree', 'id':'6'}]
+#items = []
+
+item = {'name':'Earl grey', 'description':'not grey, but good', 'price':'$2.99', 'course':'Side', 'id':'1'}
 
 @app.route('/')
 @app.route('/restaurants/')
 def showRestaurants():
-	return "This page will show all my restaurants"
+	return render_template('restaurants.html', restaurants = restaurants)
 
 @app.route('/restaurant/new/')
 def newRestaurant():
-	return "This page will be for making a new restaurant"
+	return render_template('newRestaurant.html')
 
 @app.route('/restaurant/<int:restaurant_select>/edit/')
 def editRestaurant(restaurant_select):
-	return "This page will be for editing restaurant %s" % restaurant_select
+	return render_template('editRestaurant.html', restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_select>/delete/')
 def deleteRestaurant(restaurant_select):
-	return "This page will be for deleting restaurant %s" % restaurant_select
+	return render_template('deleteRestaurant.html', restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_select>/')
 @app.route('/restaurant/<int:restaurant_select>/menu/')
 def showMenu(restaurant_select):
-	return "This page is the menu for restaurant %s" % restaurant_select
+	return render_template('menu.html', restaurant = restaurant, items = items)
 
 @app.route('/restaurant/<int:restaurant_select>/menu/new')
 def newMenuItem(restaurant_select):
-	return "This page is for making a new menu item for restaurant %s" % restaurant_select
+	return render_template('newMenuItem.html', restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_select>/menu/<int:menu_select>/edit')
 def editMenuItem(restaurant_select, menu_select):
-	return "This page is for editing menu item %s" % menu_select
+	return render_template('editMenuItem.html', restaurant = restaurant, item = item)
 
 @app.route('/restaurant/<int:restaurant_select>/menu/<int:menu_select>/delete')
 def deleteMenuItem(restaurant_select, menu_select):
-	return "This page is for deleting menu item %s" % menu_select
+	return render_template('deleteMenuItem.html', restaurant = restaurant, item = item)
 
 
 
