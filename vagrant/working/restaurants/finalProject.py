d3996b7bc417ad55f2136c99ae19f3f9ccd1abe6
unknown <dont@emailme.ever>
2016-03-05 16:07:00 -0500

    Added database queries for GET requests. Updated templates to check for empty Menu Item results.

--- vagrant/working/restaurants/finalProject.py
+++ vagrant/working/restaurants/finalProject.py
@@ -1,21 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 
-#Fake Restaurants
-restaurant = {'name': 'Sandras Tea Parlor', 'id': '1'}
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
 
-restaurants = [{'name': 'Sandras Tea Parlor', 'id': '1'}, {'name': 'Ryans specialty pesto', 'id': '2'}, {'name': 'The waffle cart', 'id': '3'}]
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+RESsession = sessionmaker(bind=engine)
+session = RESsession()
+
+
+
+
+#Fake Restaurants
+#restaurant = {'name': 'Sandras Tea Parlor', 'id': '1'}
+
+#restaurants = [{'name': 'Sandras Tea Parlor', 'id': '1'}, {'name': 'Ryans specialty pesto', 'id': '2'}, {'name': 'The waffle cart', 'id': '3'}]
 #restaurants = []
 
-items = [{'name':'Earl grey', 'description':'not grey, but good', 'price':'$2.99', 'course':'Side', 'id':'1'}, {'name':'Pepermint', 'description':'Ryans favorite', 'price':'$2.50', 'course':'Side', 'id':'2'}, {'name':'Basil Pesto', 'description':'standard pesto', 'price':'$4.99', 'course':'Side', 'id':'3'}, {'name':'Cashew Pesto', 'description':'nutty', 'price':'$5.99', 'course':'Side', 'id':'4'}, {'name':'Belgian Waffle', 'description':'With real maple syrup', 'price':'$3.50', 'course':'Entree', 'id':'5'}, {'name':'Chocolate Waffle', 'description':'Covered in delicious chocolate', 'price':'$4.50', 'course':'Entree', 'id':'6'}]
+#items = [{'name':'Earl grey', 'description':'not grey, but good', 'price':'$2.99', 'course':'Side', 'id':'1'}, {'name':'Pepermint', 'description':'Ryans favorite', 'price':'$2.50', 'course':'Side', 'id':'2'}, {'name':'Basil Pesto', 'description':'standard pesto', 'price':'$4.99', 'course':'Side', 'id':'3'}, {'name':'Cashew Pesto', 'description':'nutty', 'price':'$5.99', 'course':'Side', 'id':'4'}, {'name':'Belgian Waffle', 'description':'With real maple syrup', 'price':'$3.50', 'course':'Entree', 'id':'5'}, {'name':'Chocolate Waffle', 'description':'Covered in delicious chocolate', 'price':'$4.50', 'course':'Entree', 'id':'6'}]
 #items = []
 
-item = {'name':'Earl grey', 'description':'not grey, but good', 'price':'$2.99', 'course':'Side', 'id':'1'}
+#item = {'name':'Earl grey', 'description':'not grey, but good', 'price':'$2.99', 'course':'Side', 'id':'1'}
 
 @app.route('/')
 @app.route('/restaurants/')
 def showRestaurants():
+	restaurants = session.query(Restaurant)
 	return render_template('restaurants.html', restaurants = restaurants)
 
 @app.route('/restaurant/new/')
@@ -24,27 +37,36 @@ def newRestaurant():
 
 @app.route('/restaurant/<int:restaurant_select>/edit/')
 def editRestaurant(restaurant_select):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_select).one()
 	return render_template('editRestaurant.html', restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_select>/delete/')
 def deleteRestaurant(restaurant_select):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_select).one()
 	return render_template('deleteRestaurant.html', restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_select>/')
 @app.route('/restaurant/<int:restaurant_select>/menu/')
 def showMenu(restaurant_select):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_select).one()
+	items = session.query(MenuItem).filter_by(restaurant_id = restaurant_select)
 	return render_template('menu.html', restaurant = restaurant, items = items)
 
 @app.route('/restaurant/<int:restaurant_select>/menu/new')
 def newMenuItem(restaurant_select):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_select).one()
 	return render_template('newMenuItem.html', restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_select>/menu/<int:menu_select>/edit')
 def editMenuItem(restaurant_select, menu_select):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_select).one()
+	item = session.query(MenuItem).filter_by(restaurant_id = restaurant_select).filter_by(id = menu_select).first()
 	return render_template('editMenuItem.html', restaurant = restaurant, item = item)
 
 @app.route('/restaurant/<int:restaurant_select>/menu/<int:menu_select>/delete')
 def deleteMenuItem(restaurant_select, menu_select):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_select).one()
+	item = session.query(MenuItem).filter_by(restaurant_id = restaurant_select).filter_by(id = menu_select).first()
 	return render_template('deleteMenuItem.html', restaurant = restaurant, item = item)
 
 
